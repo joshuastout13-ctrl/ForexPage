faec583f7d55d5185c069316f6a105afe9bb35c3
DigitalExpart <214820464+DigitalExpart@users.noreply.github.com>
2026-04-16 22:53:23 +0100

    Cleanup: Remove node_modules and scratch from history, update gitignore

--- scratch/dump-true-user.js
+++
@@ -1,9 +0,0 @@
-import { readSheet } from '../lib/sheets.js';
-import { CONFIG } from '../lib/config.js';
-
-async function dumpTrue() {
-  const rows = await readSheet(CONFIG.tabs.investors);
-  const user = rows.find(r => (r.portalusername ?? r.username) === "TRUE");
-  console.log(JSON.stringify(user, null, 2));
-}
-dumpTrue();
